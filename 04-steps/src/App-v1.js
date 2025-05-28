54b61ae7e4190d05d5356ba281fededb23e1425e
salemHb <husseinsalim419@gmail.com>
2025-05-28 13:38:38 +0300

    Reusability with the cildren prop

--- 04-steps/src/App-v1.js
+++ 04-steps/src/App-v1.js
@@ -10,6 +10,7 @@ export default function App() {
 	return (
 		<div>
 			<Steps />
+			<Steps />
 		</div>
 	);
 }
@@ -39,50 +40,25 @@ function Steps() {
 						<div className={step >= 3 ? "active" : ""}>3</div>
 					</div>
 
-					<StepMessage step={step}>
-						{messages[step - 1]}{" "}
-						<div className="buttons">
-							<Button
-								bgColor="#e7e7e7"
-								textColor="#333"
-								onClick={() => alert(`Learn how to ${messages[step - 1]}`)}
-							>
-								Learn How
-							</Button>
-						</div>
-					</StepMessage>
-
+					<p className="message">
+						Step {step}: {messages[step - 1]}
+					</p>
 					<div className="buttons">
-						<Button bgColor="#7950f2" textColor="#fff" onClick={handlePrevious}>
-							<span>⬅</span>Previous
-						</Button>
-						<Button bgColor="#7950f2" textColor="#fff" onClick={handleNext}>
-							{" "}
-							Next <span>➡</span>
-						</Button>
+						<button
+							style={{ backgroundColor: "#7950f2", color: "#fff" }}
+							onClick={handlePrevious}
+						>
+							Previous
+						</button>
+						<button
+							style={{ backgroundColor: "#7950f2", color: "#fff" }}
+							onClick={handleNext}
+						>
+							Next
+						</button>
 					</div>
 				</div>
 			)}
 		</div>
-	);
-}
-
-function StepMessage({ step, children }) {
-	return (
-		<div className="message">
-			<h3>Step {step}</h3>
-			{children}
-		</div>
-	);
-}
-
-function Button({ textColor, bgColor, onClick, children }) {
-	return (
-		<button
-			style={{ backgroundColor: bgColor, color: textColor }}
-			onClick={onClick}
-		>
-			{children}
-		</button>
 	);
 }
