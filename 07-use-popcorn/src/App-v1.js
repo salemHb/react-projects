b5c2450c3ce3782aed15a8d119b627e9e25b0a0a
salemHb <husseinsalim419@gmail.com>
2025-06-16 14:14:04 +0300

    handling errors

--- 07-use-popcorn/src/App-v1.js
+++ 07-use-popcorn/src/App-v1.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 const tempMovieData = [
 	{
@@ -50,41 +50,9 @@ const tempWatchedData = [
 const average = (arr) =>
 	arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
-const KEY = "6a5388ca";
-
 export default function App() {
-	const [movies, setMovies] = useState([]);
-	const [watched, setWatched] = useState([]);
-	const [isLoading, setIsLoading] = useState(false);
-	const [error, setError] = useState("");
-	const query = "saofhavmafvkf";
-
-	useEffect(function () {
-		async function fetchMovies() {
-			try {
-				setIsLoading(true);
-				const res = await fetch(
-					`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
-				);
-
-				if (!res.ok)
-					throw new Error("Something went wrong with fetching movies");
-
-				const data = await res.json();
-				if (data.Response === "False") throw new Error("Movie Not Found");
-
-				setMovies(data.Search);
-				console.log(data);
-			} catch (err) {
-				console.error(err.message);
-				setError(err.message);
-			} finally {
-				setIsLoading(false);
-			}
-		}
-		fetchMovies();
-	}, []);
-
+	const [movies, setMovies] = useState(tempMovieData);
+	const [watched, setWatched] = useState(tempWatchedData);
 	return (
 		<>
 			<NavBar>
@@ -93,10 +61,7 @@ export default function App() {
 			</NavBar>
 			<Main>
 				<Box>
-					{/* {isLoading ? <Loader /> : <MovieList movies={movies} />} */}
-					{isLoading && <Loader />}
-					{isLoading && error && <ErrorMessage message={error} />}
-					{error && <ErrorMessage message={error} />}
+					<MovieList movies={movies} />
 				</Box>
 				<Box>
 					<WatchedSummary watched={watched} />
@@ -104,21 +69,6 @@ export default function App() {
 				</Box>
 			</Main>
 		</>
-	);
-}
-
-function Loader() {
-	return <p className="loader">Loading...</p>;
-}
-
-function ErrorMessage({ message }) {
-	return (
-		<p className="error">
-			<span role="img" aria-label="Face with rolling eyes">
-				🙄
-			</span>
-			{message}
-		</p>
 	);
 }
 
